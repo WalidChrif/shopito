66cc00f1d0def3a5a227be69594a9ea1760f77ee
walid chrif <walid.chrif@cashplus.ma>
2024-12-19 18:12:26 +0100

    commented links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
 
 def index
     @title = "SolidQueue"
+    @products = Product.all
+    @top_sellers = Product.top_sellers
+    @new_arrivals = Product.new_arrivals
+    @best_deals = Product.best_deals
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,22 +1,18 @@
 class Product < ApplicationRecord
 
+    def initialize(attributes = {})
+      super
+      self.sales_count ||= 0
+    end
+
     validates :title, presence: true
     validates :price, presence: true
     validates :stock, presence: true
-    validates :sales_count, default: 0
-    validates :discount, presence: true
 
 
     scope :top_sellers, -> { order(sales_count: :desc).limit(3) }
     scope :new_arrivals, -> { order(created_at: :desc).limit(3) }
     scope :best_deals, -> { order(discount: :desc).limit(3) }
-
-
-    
-
-    scope :active, -> { where(active: true) }
-
-
 
 
 end
