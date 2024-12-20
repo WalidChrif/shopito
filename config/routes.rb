dbc822a718609f4aadb1cd35514379b770663283
walid chrif <walid.chrif@cashplus.ma>
2024-12-20 11:13:36 +0100

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
+  get 'products' => 'customer/products#index'
+  get 'products/:id' => 'customer/products#show'
+  
+  namespace :customer do
+    get 'products' => 'products#index', as: :products
+    get 'products/:id' => 'products#show', as: :product
+  end
 end
